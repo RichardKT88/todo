8fd131415e7d0bcebbfbe94c5c5bdd010add505d
RichardKT88 <kendy_t88@hotmail.com>
2022-07-30 06:57:28 -0300

    [Done] Menu de navegação

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const App = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h1>Meu primeiro App</h1>
+        </div>
+    )
+}
+
+export default App;
